a4480e1141ae4c7269fcf7becd6024d8680e4e2a
bqxtt <1486126243@qq.com>
2019-09-25 21:14:26 +0800

    mavenTest

---
+++ MavenTest/src/test/java/com/maventest/MavenTest/AppTest.java
@@ -0,0 +1,16 @@
+package com.maventest.MavenTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest
+{
+
+	@Test
+	public void testMain()
+	{
+		fail("Not yet implemented");
+	}
+
+}
